2f7fdf06e5b8537a5db9ac290fa74883e286fdfa
Lyra Grands-Pamphile <lyra.grands@gmail.com>
2020-10-04 21:55:54 -0400

    ip address changes

--- deploy.js
+++ deploy.js
@@ -83,66 +83,66 @@ function sshConnect() {
     console.log('Connecting to the server...');
 
     ssh
-        .connect({
-            // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-            host: '54.243.19.169',
-            username: 'ubuntu',
-            privateKey: '2hs-key.pem'
-        })
-        .then(function () {
-            console.log('SSH Connection established.');
-            console.log('Installing PM2...');
-            return installPM2();
-        })
-        .then(function () {
-            console.log('Creating `riot-express-todo-list-temp` folder.');
-            return createRemoteTempFolder();
-        })
-        .then(function (result) {
-            const failed = [];
-            const successful = [];
-            if (result.stdout) {
-                console.log('STDOUT: ' + result.stdout);
-            }
-            if (result.stderr) {
-                console.log('STDERR: ' + result.stderr);
-                return Promise.reject(result.stderr);
-            }
-            console.log('Transferring files to remote server...');
-            return transferProjectToRemote(failed, successful);
-        })
-        .then(function (status) {
-            if (status) {
-                console.log('Stopping remote services.');
-                return stopRemoteServices();
-            } else {
-                return Promise.reject(failed.join(', '));
-            }
-        })
-        .then(function (status) {
-            if (status) {
-                console.log('Updating remote app.');
-                return updateRemoteApp();
-            } else {
-                return Promise.reject(failed.join(', '));
-            }
-        })
-        .then(function (status) {
-            if (status) {
-                console.log('Restarting remote services...');
-                return restartRemoteServices();
-            } else {
-                return Promise.reject(failed.join(', '));
-            }
-        })
-        .then(function () {
-            console.log('DEPLOYMENT COMPLETE!');
-            process.exit(0);
-        })
-        .catch(e => {
-            console.error(e);
-            process.exit(1);
-        });
+			.connect({
+				// TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
+				host: '54.226.204.114',
+				username: 'ubuntu',
+				privateKey: '2hs-key.pem',
+			})
+			.then(function () {
+				console.log('SSH Connection established.');
+				console.log('Installing PM2...');
+				return installPM2();
+			})
+			.then(function () {
+				console.log('Creating `riot-express-todo-list-temp` folder.');
+				return createRemoteTempFolder();
+			})
+			.then(function (result) {
+				const failed = [];
+				const successful = [];
+				if (result.stdout) {
+					console.log('STDOUT: ' + result.stdout);
+				}
+				if (result.stderr) {
+					console.log('STDERR: ' + result.stderr);
+					return Promise.reject(result.stderr);
+				}
+				console.log('Transferring files to remote server...');
+				return transferProjectToRemote(failed, successful);
+			})
+			.then(function (status) {
+				if (status) {
+					console.log('Stopping remote services.');
+					return stopRemoteServices();
+				} else {
+					return Promise.reject(failed.join(', '));
+				}
+			})
+			.then(function (status) {
+				if (status) {
+					console.log('Updating remote app.');
+					return updateRemoteApp();
+				} else {
+					return Promise.reject(failed.join(', '));
+				}
+			})
+			.then(function (status) {
+				if (status) {
+					console.log('Restarting remote services...');
+					return restartRemoteServices();
+				} else {
+					return Promise.reject(failed.join(', '));
+				}
+			})
+			.then(function () {
+				console.log('DEPLOYMENT COMPLETE!');
+				process.exit(0);
+			})
+			.catch((e) => {
+				console.error(e);
+				process.exit(1);
+			});
 }
 
 main();
